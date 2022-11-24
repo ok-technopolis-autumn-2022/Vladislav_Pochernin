a85cba5aa1eaf73d86d45e39a2bfb7a908961512
Vladislav Pochernin <vspochernin@gmail.com>
2022-11-25 02:37:17 +0300

    part4-js: doing part4

--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
+const tasks = new Map();
+const isDone = new Map();
+
 const form = document.querySelector('.todo-app__create-new');
 const ul = document.querySelector('.todo-app__task-list');
-
-console.log(ul);
+const all = document.getElementById('all')
+const active = document.getElementById('active')
+const completed = document.getElementById('completed')
+const selectAllButton = document.querySelector('.todo-app__select-all')
+const clearCompletedButton = document.querySelector('.actions-bar__clear-completed')
 
 function addTask(e) {
     e.preventDefault();
 
-    const task = createTask(this.description.value);
-    ul.appendChild(createLi(task));
+    const taskInfo = createTask(this.description.value);
+
+    const task = createLi(taskInfo);
+    tasks.set(task.id, task);
+    isDone.set(task.id, false);
+    showTasks()
 
     this.reset();
+}
+
+function showTasks() {
+    ul.childNodes.forEach(li => {
+        li.remove();
+    });
+
+    tasks.forEach((value, key) => {
+        if (all.checked) {
+            ul.appendChild(value);
+        } else if (active.checked && isDone.get(key) === false) {
+            ul.appendChild(value);
+        } else if (completed.checked && isDone.get(key) === true) {
+            ul.appendChild(value);
+        }
+    });
+
+    updateCounter();
+}
+
+function updateCounter() {
+    let counter = 0;
+    ul.childNodes.forEach(li => {
+        if (isDone.get(li.id) === false) {
+            counter++;
+        }
+    })
+    document.querySelector('.actions-bar__active-counter').textContent = counter.toString() + ' items left';
 }
 
 /**
@@ -38,6 +76,12 @@ function createLi(task) {
     span.textContent = task.desc;
     const toggleDone = () => {
         span.classList.toggle('done');
+        if (span.classList.contains('done')) {
+            isDone.set(li.id, true);
+        } else {
+            isDone.set(li.id, false);
+        }
+        updateCounter();
     };
     label.addEventListener('click', toggleDone);
 
@@ -48,6 +92,8 @@ function createLi(task) {
     const deleteTask = () => {
         deleteButton.removeEventListener('click', deleteTask);
         li.remove();
+        tasks.delete(li.id);
+        isDone.delete(li.id);
     };
     deleteButton.addEventListener('click', deleteTask);
 
@@ -64,3 +110,35 @@ function createTask(desc) {
 }
 
 form.addEventListener('submit', addTask)
+
+function checkAllTask() {
+    ul.childNodes.forEach(li => {
+        const span = li.querySelector('.task-item__text');
+        const checkbox = li.querySelector('.task-item__status')
+        if (isDone.get(li.id) === false) {
+            if (span.classList.toggle('done')) {
+                isDone.set(li.id, true);
+                checkbox.checked = true;
+            }
+        }
+    });
+    updateCounter();
+}
+
+function clearCompleted() {
+    ul.childNodes.forEach(li => {
+        if (isDone.get(li.id) === true) {
+            li.remove();
+            tasks.delete(li.id);
+            isDone.delete(li.id)
+        }
+    });
+}
+
+clearCompletedButton.addEventListener('click', clearCompleted);
+
+selectAllButton.addEventListener('click', checkAllTask);
+
+all.addEventListener('click', showTasks);
+active.addEventListener('click', showTasks);
+completed.addEventListener('click', showTasks);
